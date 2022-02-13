27b9392695697cd6f8bfca33838a93d7eb9aa0d2
asif a owadud <asifaowadud@gmail.com>
2022-02-13 13:28:06 +0600

    130220221327

--- Basic JS on HTML-CSS-DOM/pHeroProjects/Projects - 3 shopping-cart-main/app.js
+++ Basic JS on HTML-CSS-DOM/pHeroProjects/Projects - 3 shopping-cart-main/app.js
@@ -1,22 +1,47 @@
 let caseQtyPlusBtn = document.querySelector('#caseQtyPlus');
 let caseQtyMinusBtn = document.querySelector('#caseQtyMinus');
+let mobileQtyPlusBtn = document.querySelector('#mobileQtyPlus');
+let mobileQtyMinusBtn = document.querySelector('#mobileQtyMinus');
 
-function updateQtyNPrice() {
 
-    let caseQtyTag = document.querySelector('#caseQty');
-    let caseQty = Number(caseQtyTag.value);
-    caseQtyTag.value = Number(caseQty + 1);
+function updateQtyNPrice(product, price, isPlusQty) {
+
+    let QtyTag = document.querySelector(`#${product}Qty`);
+    let Qty = Number(QtyTag.value);
+
+    if (isPlusQty == true) {
+        QtyTag.value = Number(Qty + 1);
+    } else if (Qty > 0) {
+        QtyTag.value = Number(Qty - 1);
+    }
 
     // update case price
-    let casePriceTag = document.querySelector('#casePriceTotal');
-    let casePrice = Number(casePriceTag.innerHTML);
-    casePriceTag.innerHTML = Number(59 * caseQtyTag.value);
+    let PriceTag = document.querySelector('#' + product + 'PriceTotal');
+    // let casePrice = Number(PriceTag.innerHTML);
+    PriceTag.innerHTML = Number(price * QtyTag.value);
 }
+// for mobile qty n price update
+mobileQtyPlusBtn.addEventListener('click', function () {
+    updateQtyNPrice("mobile", 1219, true)
+});
 
+mobileQtyMinusBtn.addEventListener('click', function () {
+    updateQtyNPrice("mobile", 1219, false)
+});
+
+
+
+// for case qty n price update
 caseQtyPlusBtn.addEventListener('click', function () {
-    updateQtyNPrice()
+    updateQtyNPrice("case", 59, true)
 });
 
 caseQtyMinusBtn.addEventListener('click', function () {
-    updateQtyNPrice()
+    updateQtyNPrice("case", 59, false)
 });
+
+
+// null valueing btn
+/* function zeroQtyNPrice(product) {
+    document.getElementById(`#${product}Qty`).value = 0;
+} */
