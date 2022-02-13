f379ed6b89c5b64b4e76ff6034e80ccab447d3e9
asif a owadud <asifaowadud@gmail.com>
2022-02-13 11:44:03 +0600

    130220221144

--- Basic JS on HTML-CSS-DOM/pHeroProjects/Projects - 3 shopping-cart-main/app.js
+++ Basic JS on HTML-CSS-DOM/pHeroProjects/Projects - 3 shopping-cart-main/app.js
@@ -1,1 +1,22 @@
-console.log('I am in a separate file')
+let caseQtyPlusBtn = document.querySelector('#caseQtyPlus');
+let caseQtyMinusBtn = document.querySelector('#caseQtyMinus');
+
+function updateQtyNPrice() {
+
+    let caseQtyTag = document.querySelector('#caseQty');
+    let caseQty = Number(caseQtyTag.value);
+    caseQtyTag.value = Number(caseQty + 1);
+
+    // update case price
+    let casePriceTag = document.querySelector('#casePriceTotal');
+    let casePrice = Number(casePriceTag.innerHTML);
+    casePriceTag.innerHTML = Number(59 * caseQtyTag.value);
+}
+
+caseQtyPlusBtn.addEventListener('click', function () {
+    updateQtyNPrice()
+});
+
+caseQtyMinusBtn.addEventListener('click', function () {
+    updateQtyNPrice()
+});
