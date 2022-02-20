f740ba78054983c82bce75cb3f8bec2f84ac6aac
asif a owadud <asifaowadud@gmail.com>
2022-02-20 16:43:04 +0600

    20-02-2022 16:42

--- Basic JS on HTML-CSS-DOM/domTheory.js
+++ Basic JS on HTML-CSS-DOM/domTheory.js
@@ -814,4 +814,27 @@ userInput.addEventListener("keyup", function () {
 
 
 
-// *************************** 27-5 (advanced) Common function to reduce code repeat
+// *************************** 32-1 Module Introduction and Basic ES6 Recap+
+
+// -reject "var" use "let" and "const"
+const hubby = 'Omor Sani';
+let phone = 'iphone 15';
+phone = 'Samsung Galaxy S17';
+// 2. default parameter-VALUE AND SPREAD OPERATOR or three dots (...) 
+function maxNumber(array = []) {
+    const max = Math.max(...array);
+    return max;
+}
+const biggest = maxNumber();
+console.log(biggest);
+
+// 3. template string
+const myNotes = `I am mojnu of ${hubby}. I have a ${phone}.`
+console.log(myNotes);
+
+// 4. arrow function
+/* function square(x) {
+    return x * x;
+} */
+const square = x => x * x;
+console.log(square(9));
