1adcad7f4ebcb12ea18bd387e24849d6c8c5d907
asif a owadud <asifaowadud@gmail.com>
2022-02-25 18:22:52 +0600

    25-02-2022 08:44

--- Basic JS on HTML-CSS-DOM/domTheory.js
+++ Basic JS on HTML-CSS-DOM/domTheory.js
@@ -1901,3 +1901,78 @@ function photosdisplayOnHtml(params) {
 //         console.log(param);
 //     }
 // } */
+
+
+
+
+
+// [[[[[[[[[[[[[[[[[[[[[[[[[[Module : 35  ||| 9 videos]]]]]]]]]]]]]]]]]]]]]]]]]]
+
+
+
+
+
+
+
+// ********************************** 35-1 Module Overview, Dynamic data types
+
+
+
+// JavaScript একটা dynamic data types supported language. এর মানে হল কোন specific variable এর value এর data type যদি dynamically change হতে থাকে তবুও JavaScript এ কোন সমস্যা হয় না সে নিজে নিজেই তা বুঝে নিতে পারে।
+
+let a = 19;
+console.log(`now typeof variable a is, "${typeof a}"`);
+a = 'Jamal Uddin Shahen Shah';
+console.log(`now typeof variable a is, "${typeof a}"`);
+a = false;
+console.log(`now typeof variable a is, "${typeof a}"`);
+
+
+
+
+
+// ********************************** 35-2 Primitive data type and non-primitive data type
+
+
+
+
+/* in a broadsense dataType 2 প্ রকার(i) primitive, (ii) non - primitive Data types
+
+primitive data types  এরা আবার ৬ প্রকার ,
+1. number
+2. string
+3. boolean
+4. undefined
+5. null
+6. symbol
+
+(১) যাদের একসাথে মাত্র একটা value ই থাকতে পারে 
+(২) এরা memory  storage এর reference ধরে রাখে যেমন, */
+
+/* let a = 'hello'; 
+let b = a; // এখানে b এর value হিসেবে a এর value কে assign হয়েছে
+console.log(a, b);
+a = 'gello'; // a এর value chenge হয়েছে হিসেবে তাহলে b এর value ও চেঞ্জ হবার কথা কারণ এরা memory  storage এর reference ধরে রাখে
+console.log(a, b); //  */
+
+
+/*
+non - primitive এরা আবার 2 প্রকার ,
+1. object
+2. ARRAY
+
+(১) যাদের একসাথে মাত্র একটা value ই থাকতে পারে 
+(২) এরা memory  storage এর reference ধরে রাখে যেমন, */
+
+
+
+/* const x = {
+    job: 'web developer'
+}
+const y = x;
+console.log(x, y);
+
+x.job = 'front end developer'; // যেকোন variable এর value change করলেই দুটারই value change  হয়ে যাছে
+console.log(x, y);
+y.job = 'backend end developer'; // যেকোন variable এর value change করলেই দুটারই value change  হয়ে যাছে
+console.log(x, y); */
