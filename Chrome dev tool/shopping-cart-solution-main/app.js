0b067d3bb141fe028ae5555853c0b4c8eda4e3ce
asif a owadud <asifaowadud@gmail.com>
2022-03-05 15:47:21 +0600

    05-03-2022 15:27

--- Chrome dev tool/shopping-cart-solution-main/app.js
+++ Chrome dev tool/shopping-cart-solution-main/app.js
@@ -3,8 +3,7 @@ function updateProductNumber(product, price, isIncreasing) {
     let productNumber = productInput.value;
     if (isIncreasing == true) {
         productNumber = parseInt(productNumber) + 1;
-    }
-    else if (productNumber > 0) {
+    } else if (productNumber > 0) {
         productNumber = parseInt(productNumber) - 1;
     }
     productInput.value = productNumber;
