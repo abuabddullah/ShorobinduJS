a6f8ff19d1a2abbe5b35dc06904cfa78ab6ce8ed
asif a owadud <asifaowadud@gmail.com>
2022-03-15 17:52:21 +0600

    15-03-2022 13:52

---
+++ 2-React js/react.js
@@ -0,0 +1,54 @@
+console.log(`[[[[[[[[[[[[[[[[[[[[[[[[[[[[[Module 44 || Videos 9]]]]]]]]]]]]]]]]]]]]]]]]]]]]]`);
+
+
+
+
+console.log(`       // ************************** 44-1 Module overview and modern application core parts    `);
+
+
+
+// thins to remember (watch JavaScript for React video)
+/* 
+-Arrow function
+-Spread operator
+-Template String
+-Array methods : Map, forEach, filter,find
+-Destruturing of object
+-Key, value pairs of object
+-Optional Chaining
+-Ternary operator
+-JSON, Fetch, Async-await
+-Short cut and Short cut or (i.e Logical and || Logical or)
+-Truthy values || Falsy values
+-Default parameters
+-Local Storage and Session Storage
+
+
+// need to get ideas about the core parts of a modern application
+-Component
+-Template
+-State
+-Routing
+ */
+
+
+
+console.log(`   // ************************** 44-2 Web component, 4 types of component, identify component  `);
+
+
+
+
+// There are four types of component
+/* 
+(1) "similar in look but differernt in data" type component [in short 4 me "SLDD"].
+ eg: blog-posts in an article
+ 
+(2) "container" type component that holds other "similar in look but differernt in data" type components.
+ eg: article that holding blog-posts
+
+(3) not "SLDD" nor "container" type component "No repeating pattern but breakdown for working purpose" [in short 4 me "No Rpeat Component"].
+ eg: side bar of a website
+
+ (4) "Stand Alone" type component that is unique for rest components of a website.
+ eg: header,footer,sign-in parts of a website
+ */
